f01e7c7ac6b1d99f62bf87eca77d5e462a17f7e4
RiderExMachina <riderexmachina@protonmail.com>
2024-06-01 02:27:27 -0700

    Test buttons now appear

--- main-qt.py
+++ main-qt.py
@@ -14,7 +14,9 @@ from PyQt6.QtWidgets import (
 ## Constants
 WINDOW_SIZE = 800
 
-
+def loadItems():
+    itemList = ["Provoxin", "Anatolin", "Dog Tags", "Gauze Bandages"]
+    return itemList
 
 class itemSelect(QMainWindow):
     def __init__(self):
@@ -25,15 +27,28 @@ class itemSelect(QMainWindow):
         centralWidget = QWidget(self)
         centralWidget.setLayout(self.generalLayout)
         self.setCentralWidget(centralWidget)
-    
+        self._getItems()
+
     def _getItems(self):
-        loadItems()
+        COLUMNS = 3
+        BUTTON_HEIGHT = 175
+        BUTTON_WIDTH = 225
+        self.buttonGrid = {}
+        button_layout = QGridLayout()
+        itemList = loadItems()
+        for col in range(0, COLUMNS):
+            for row, item in enumerate(itemList):
+                self.buttonGrid[item] = QPushButton(f"{item} - {row}, {col}")
+                self.buttonGrid[item].setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
+                button_layout.addWidget(self.buttonGrid[item], row, col)
+        self.generalLayout.addLayout(button_layout)
+
 
 class itemEditor(QMainWindow):
     
-    def _createButtons(self):
+    def _createNumberButtons(self):
         self.buttonMap = {}
-        number_pad = QGridLayout
+        number_pad = QGridLayout()
         keyboard = [
             ["1", "2", "3"],
             ["4", "5", "6"],
