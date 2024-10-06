RiderExMachina/SIMple

commit dccaed113a37572db489ee85294c4dd712de1a4f

Papercuts (#22)
* Fix various small papercuts

- Too many to list, but closes #17 and closes #5

inventory/app.py
@@ -8,7 +8,6 @@ from pathlib import Path
 # imports - third party imports
 from flask import Flask, redirect, render_template, request
 
-
 DATABASE_NAME = "inventory.sqlite"
 _DATABASE_PATH = Path(__file__).parent.parent / DATABASE_NAME
 VIEWS = {
@@ -100,49 +99,57 @@ def product():
 
     with sqlite3.connect(DATABASE_NAME) as conn:
         if request.method == "POST":
+            ## product = get_data(request)
             (prod_name,
             prod_upc,
-            quantity,
-            quick_take_qty, 
-            reorder_qty, 
+            prod_quantity,
+            quick_take_qty,
+            reorder_qty,
             restock_qty,
             location,
             categories) = (
-                        request.form["prod_name"], 
+                        request.form["prod_name"],
                         request.form["prod_upc"],
-                        request.form["prod_quantity"], 
+                        request.form["prod_quantity"],
                         request.form["quick_take_qty"],
-                        request.form["reorder_qty"], 
+                        request.form["reorder_qty"],
                         request.form["restock_qty"],
                         request.form["location"],
                         request.form["categories"]
                         )
             ## Verify Data
             transaction_allowed = True
-            try:
-                quantity = int(quantity)
-                quick_take_qty = int(quick_take_qty)
-                restock_qty = int(restock_qty)
-                reorder_qty = int(reorder_qty)
-            except ValueError:
-                print("Expected integer, did not recieve one.")
-                transaction_allowed = False
-            
-            for to_check in [prod_name, prod_upc, quantity, quick_take_qty, reorder_qty, restock_qty, location]:
-                if to_check in EMPTY_SYMBOLS:
+            for value in [prod_name, prod_upc, prod_quantity, quick_take_qty, reorder_qty, restock_qty, location]:
+                if value in EMPTY_SYMBOLS:
                     transaction_allowed = False
-            for num_validation in [quantity, quick_take_qty, reorder_qty, restock_qty]:
+                    error_type = "Required Field Left Empty"
+                    return render_template(
+                                    'modal.jinja',
+                                    link=VIEWS,
+                                    error_code=error_type,
+                                    transaction_message=f"Unable to set required field, {name}",
+                                    previous=VIEWS["Stock"]
+                                )
+            prod_quantity = int(prod_quantity)
+            quick_take_qty = int(quick_take_qty)
+            reorder_qty = int(reorder_qty)
+            restock_qty = int(restock_qty)
+            for num_validation in [prod_quantity, quick_take_qty, reorder_qty, restock_qty]:
                 if num_validation < 0:
                     transaction_allowed = False
+                    error_type = "Negative Values"
                     return render_template(
-                        'modal.jinja',
-                        transaction_message=f"Please do not enter negative numbers."
-                    )
+                                    'modal.jinja',
+                                    link=VIEWS,
+                                    error_code=error_type,
+                                    transaction_message=f"Unable to set {name}. Value '{value}' is invalid.",
+                                    previous=VIEWS["Stock"]
+                                )
 
             if transaction_allowed:
                 conn.execute(
                     "INSERT INTO products (prod_name, prod_upc, prod_quantity, quick_take_qty, reorder_qty, restock_qty, location, categories) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-                    (prod_name, prod_upc, quantity, quick_take_qty, reorder_qty, restock_qty, location, categories),
+                    (prod_name, prod_upc, prod_quantity, quick_take_qty, reorder_qty, restock_qty, location, categories),
                 )
                 return redirect(VIEWS["Stock"])
 
@@ -342,11 +349,9 @@ def delete():
 @app.route("/quick-change", methods=["GET", "POST"])
 def quick_change():
     def update_qty(prod_id, qty):
-        if qty < 0:
-            qty = 0
         conn.execute(
                 "UPDATE products SET prod_quantity = ? WHERE prod_id = ?",
-                (new_qty,  prod_id),
+                (qty,  prod_id),
             )
 
     quick_change_type = request.args.get("type")
@@ -362,10 +367,7 @@ def quick_change():
             request.args.get("qty")
         )
     if qty:
-        try:
-            qty = int(qty)
-        except:
-            print(f"Failure: {qty} is not an integer")
+        qty = int(qty)
 
     with sqlite3.connect(DATABASE_NAME) as conn:
         old_prod_quantity = conn.execute(
@@ -374,13 +376,20 @@ def quick_change():
         match quick_change_type:
             case "subtract":
                 new_qty = old_prod_quantity - qty
+                if new_qty < 0:
+                    new_qty = 0
                 update_qty(prod_id, new_qty)
             case "add":
                 new_qty = old_prod_quantity + qty
                 update_qty(prod_id, new_qty)
             case "reorder":
+                get_state = conn.execute("SELECT been_reordered FROM products WHERE prod_id = ?", (prod_id)).fetchone()[0]
+                if get_state == 1:
+                    new_state = 0
+                elif get_state == 0:
+                    new_state = 1
                 conn.execute(
-                    "UPDATE products SET been_reordered = 1 WHERE prod_id = ?", (prod_id)
+                    "UPDATE products SET been_reordered = ? WHERE prod_id = ?", (new_state, prod_id)
                     )
             case "restock":
                 restock_qty = conn.execute("SELECT restock_qty FROM products WHERE prod_id = ?", (prod_id)).fetchone()[0]
@@ -397,6 +406,12 @@ def quick_change():
 
 @app.route("/edit", methods=["POST"])
 def edit():
+    def update_db(column, value, prod_id):
+        conn.execute(
+                    f"UPDATE products SET {column} = ? WHERE prod_id = ?",
+                    (value, prod_id),
+                    )
+
     edit_record_type = request.args.get("type")
 
     with sqlite3.connect(DATABASE_NAME) as conn:
@@ -419,69 +434,54 @@ def edit():
                 restock_qty,
                 location,
                 categories) = (
-                            request.form["prod_id"],
-                            request.form["prod_name"],
-                            request.form["prod_upc"],
-                            request.form["prod_quantity"],
-                            request.form["quick_take_qty"],
-                            request.form["reorder_qty"],
-                            request.form["restock_qty"],
-                            request.form["location"],
-                            request.form["categories"]
+                            ["Product ID", "prod_id", request.form["prod_id"]],
+                            ["Product Name", "prod_name", request.form["prod_name"]],
+                            ["Product UPC", "prod_upc", request.form["prod_upc"]],
+                            ["Product Quantity", "prod_quantity", request.form["prod_quantity"]],
+                            ["Product Quick Take", "quick_take_qty", request.form["quick_take_qty"]],
+                            ["Minimum Quantity", "reorder_qty", request.form["reorder_qty"]],
+                            ["Restock Quantity", "restock_qty", request.form["restock_qty"]],
+                            ["Location", "location", request.form["location"]],
+                            ["Categories", "categories", request.form["categories"]]
                             )
                 ## Validate Data
-                for to_check in [prod_quantity, quick_take_qty, reorder_qty, restock_qty]:
-                    if to_check not in EMPTY_SYMBOLS:
-                        try:
-                            if int(to_check) < 0:
-                                return render_template(
-                                    'modal.jinja',
-                                    transaction_message=f"Please do not enter negative numbers."
-                            )
-                        except ValueError:
-                            print("Expected integer, did not recieve one.")
-                        
+                for name, _, value in [prod_quantity, quick_take_qty, reorder_qty, restock_qty]:
+                    if value not in EMPTY_SYMBOLS:
+                        if int(value) < 0:
+                            error_type = "Negative Values"
+                            return render_template(
+                                            'modal.jinja',
+                                            link=VIEWS,
+                                            error_code=error_type,
+                                            transaction_message=f"Unable to change {name}. Value '{value}' is invalid.",
+                                            previous=VIEWS["Stock"]
+                                        )
 
-                if prod_name:
-                    conn.execute(
-                        "UPDATE products SET prod_name = ? WHERE prod_id = ?",
-                        (prod_name, prod_id),
-                    )
-                if prod_upc:
-                    conn.execute(
-                        "UPDATE products SET prod_upc = ? WHERE prod_id = ?",
-                        (prod_upc, prod_id)
-                        )
-                if prod_quantity:
-                    conn.execute(
-                        "UPDATE products SET prod_quantity = ? WHERE prod_id = ?",
-                        (int(prod_quantity), prod_id),
-                    )
-                if quick_take_qty:
-                    conn.execute(
-                        "UPDATE products SET quick_take_qty = ? WHERE prod_id = ?",
-                        (int(quick_take_qty), prod_id),
-                    )
-                if reorder_qty:
-                    conn.execute(
-                            "UPDATE products SET reorder_qty = ? WHERE prod_id = ?",
-                            (int(reorder_qty), prod_id),
-                    )
-                if restock_qty:
-                    conn.execute(
-                            "UPDATE products SET restock_qty = ? WHERE prod_id = ?",
-                            (int(restock_qty), prod_id),
-                    )
-                if location:
-                    conn.execute(
-                        "UPDATE products SET location = ? WHERE prod_id = ?",
-                        (location, prod_id),
-                    )
-                if categories:
-                    conn.execute(
-                        "UPDATE products SET categories = ? WHERE prod_id = ?",
-                        (categories, prod_id),
-                    )
+                item = prod_id[2]
+                if prod_name[2]:
+                    _, column, value = prod_name
+                    update_db(column, value, item)
+                if prod_upc[2]:
+                    _, column, value = prod_upc
+                    update_db(column, value, item)
+                if prod_quantity[2]:
+                    _, column, value = prod_quantity
+                    update_db(column, value, item)
+                if quick_take_qty[2]:
+                    _, column, value = quick_take_qty
+                    update_db(column, value, item)
+                if reorder_qty[2]:
+                    _, column, value = reorder_qty
+                    update_db(column, value, item)
+                if restock_qty[2]:
+                    _, column, value = restock_qty
+                    update_db(column, value, item)
+                if location[2]:
+                    _, column, value = location
+                    update_db(column, value, item)
+                if categories[2]:
+                    _, column, value = categories
+                    update_db(column, value, item)
 
                 return redirect(VIEWS["Stock"])
 

inventory/static/main.css
@@ -1,5 +1,5 @@
-.subtract{
-    background-image= url("ui/minus-button.svg")
+.content{
+    margin-left: 25px;
 }
 
 #modal-disclaimer {
@@ -7,8 +7,16 @@
     font-size: .8em;
 }
 #reorder-icon{
-    height: 25px;
-    width: 30px;
+    height: 20px;
+    width: 25px;
+}
+
+.small-table{
+    max-width: 60%
+}
+
+.needs-space{
+    margin: 5px;
 }
 
 .new-item{

inventory/static/main.js
@@ -26,13 +26,13 @@ function enable_advanced(items){
 
     chunk = "<table>" +
             "<tr>" +
-            "<td>Vendor:</td><td><input name='vendor' placeholder="+ vendor_name +"></td>" +
+            "<td>Vendor:</td><td><input name='vendor' placeholder="+ vendor_name +" autocomplete='off'></td>" +
             "</tr><tr>" +
-            "<td>Reorder from:</td><td><input name='vendor_url' placeholder="+ vendor_url +"></td>" +
+            "<td>Reorder from:</td><td><input name='vendor_url' placeholder="+ vendor_url +" autocomplete='off'></td>" +
             "</tr><tr>" +
-            "<td>Purchase Cost:</td><td><input name='purchase_cost' placeholder="+ purchase_cost+"></td>" +
+            "<td>Purchase Cost:</td><td><input name='purchase_cost' placeholder="+ purchase_cost+" autocomplete='off'></td>" +
             "</tr><tr>" +
-            "<td>Sale Price:</td><td><input name='sale_price' placeholder="+ sale_price +"></td>" +
+            "<td>Sale Price:</td><td><input name='sale_price' placeholder="+ sale_price +" autocomplete='off'></td>" +
             "</tr>" +
             "</table>"
 
@@ -72,7 +72,7 @@ function compose_number(input){
 }
 
 function enable_custom(prod_id, type){
-   chunk ='<input name="custom_qty" id="custom-number-box" class="custom-number-edit-box"/>'
+   chunk ='<input name="custom_qty" id="custom-number-box" class="custom-number-edit-box needs-space"/>'
    add = '<input name="txn_type" value="add" hidden><button class="btn btn-success" type="submit">Add</button>'
    subtract = '<input name="txn_type" value="subtract" hidden><button class="btn btn-danger" type="submit">Take</button>'
 
@@ -84,12 +84,12 @@ function enable_custom(prod_id, type){
     }
     document.getElementById("display-"+prod_id).innerHTML = chunk;
 
-    keypad = generate_keypad()
+    keypad = generate_keypad(prod_id)
     document.getElementById("custom-insert-"+ prod_id).innerHTML = keypad;
 
 }
 
-function generate_keypad(){
+function generate_keypad(prod_id){
     info = "<table><tr>"
     for (i = 1; i <= 9; i++){
         blurb = '<td><a href="javascript:compose_number('+ i +')"><button class="btn custom-number">'+ i +'</button></a></td>'
@@ -98,11 +98,17 @@ function generate_keypad(){
         }
         info += blurb
     }
-    info += "</table>"
+    info += '</tr> <tr> <td></td>' +
+    '<td><a href="javascript:compose_number(0)"><button class="btn custom-number">0</button></a></td>'+
+    '<td></td></tr></table> <table><tr>' + // Close previous table so below buttons don't affect the nice grid'
+    '<td><a href="javascript:clear_number()"><button class="btn custom-number-text">Clear</button></a></td>' +
+    '<td><a href="javascript:disable_custom('+ prod_id +')"><button class="btn custom-number-text">Cancel</button></a></td>' +
+    '</tr> </table>'
     return info
 }
 
 
 function disable_custom(prod_id){
-
+    document.getElementById("display-"+prod_id).innerHTML = "";
+    document.getElementById("custom-insert-"+ prod_id).innerHTML = "";
 }

inventory/templates/base-template.jinja
@@ -6,6 +6,7 @@
         <link rel="stylesheet" href="{{ url_for('static', filename='bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ url_for('static', filename='main.css') }}">
         <link rel="icon" href="{{ url_for('static', filename='site_icon.png') }}" class="js-favicon">
+        <link rel="preload" href="{{url_for('static', filename='reordered.png')}}" as="image" type="image/png" />
         <!--div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div-->
         <script src="{{ url_for('static', filename='main.js') }}"></script>
         <script src="{{ url_for('static', filename='jquery-3.3.1.slim.min.js') }}"></script>

inventory/templates/index.jinja
@@ -57,34 +57,63 @@
                 <div id="edit_{{ prod_id }}" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="trans_message" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
-                        <div class="modal-body"><h3>{{ prod_name }}</h3></div>
-                        <div class="modal-body"><h4>On hand: {{ prod_qty }} - Reorder Amount: {{ reorder_qty }}</h4></div>
-                        <div class="modal-body">{{ need_reordered }} </div>
                         <div class="modal-body">
-                            <a href="{{ url_for('quick_change', product=prod_id, type='subtract', qty=1) }}">
-                                <button name="single-quick" class="btn btn-danger" align="left" value="subtract-one">Take 1</button>
-                            </a>
-                            <a href="{{ url_for('quick_change', product=prod_id, type='add', qty=1) }}">
-                                <button name="single-quick" class="btn btn-success" align="right" value="add-one">Add 1</button>
-                            </a>
+                            <h3>{{ prod_name }}</h3>
+                            <table class="table small-table">
+                                <tr>
+                                    <th>On hand</th>
+                                    <th>Minimum</th>
+                                    <th>Restock</th>
+                                </tr>
+                                <tr>
+                                    <td>{{ prod_qty }}</td>
+                                    <td>{{ min_qty }}</td>
+                                    <td>{{ reorder_qty }}</td>
+                                </tr>
+                                </table>
+                            <h5>{{ need_reordered }}</table>
+                        </div>
+                        <div class="modal-body">
+                        <table>
+                            <tr align="center">
+                                <td>
+                                    <a href="{{ url_for('quick_change', product=prod_id, type='subtract', qty=1) }}">
+                                        <button name="single-quick" class="btn btn-danger needs-space" align="left" value="subtract-one">Take 1</button>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="{{ url_for('quick_change', product=prod_id, type='add', qty=1) }}">
+                                        <button name="single-quick" class="btn btn-success needs-space" align="right" value="add-one">Add 1</button>
+                                    </a>
+                                <td>
+                            </tr>
                             {% if quick_take_qty > 1 %}
-                            <br/>
-                                <a href="{{ url_for('quick_change', product=prod_id, type='subtract', qty=quick_take_qty ) }}">
-                                <button name="single-quick" class="btn btn-danger" align="left" value="subtract-quick-take">Take {{quick_take_qty}}</button>
-                            </a>
-                            <a href="{{ url_for('quick_change', product=prod_id, type='add', qty=quick_take_qty) }}">
-                                <button name="single-quick" class="btn btn-success" align="right" value="add-quick-take">Add {{ quick_take_qty }}</button>
-                            </a>
-                            <br/>
-                            {% else %}
-                            <br>
+                            <tr align="center">
+                                <td>
+                                    <a href="{{ url_for('quick_change', product=prod_id, type='subtract', qty=quick_take_qty ) }}">
+                                    <button name="single-quick" class="btn btn-danger needs-space" align="left" value="subtract-quick-take">Take {{quick_take_qty}}</button>
+                                </a>
+                                </td>
+                                <td>
+                                    <a href="{{ url_for('quick_change', product=prod_id, type='add', qty=quick_take_qty) }}">
+                                        <button name="single-quick" class="btn btn-success" align="right" value="add-quick-take needs-space">Add {{ quick_take_qty }}</button>
+                                    </a>
+                                </td>
+                            </tr>
                             {% endif %}
-                            <a href="javascript:enable_custom({{prod_id}}, 'subtract')" id="custom-subtract-{{prod_id}}">
-                                <button name="single-quick" class="btn btn-danger" align="left" value="subtract-custom">Take Other</button>
-                            </a>
-                            <a href="javascript:enable_custom({{prod_id}}, 'add')" id="custom-subtract-{{prod_id}}">
-                                <button name="single-quick" class="btn btn-success" align="right" value="add-custom">Add other</button>
-                            </a>
+                            <tr align="center">
+                                <td>
+                                    <a href="javascript:enable_custom({{prod_id}}, 'subtract')" id="custom-subtract-{{prod_id}}">
+                                        <button name="single-quick" class="btn btn-danger needs-space" align="left" value="subtract-custom">Take Other</button>
+                                    </a>
+                                </td>
+                                <td>
+                                    <a href="javascript:enable_custom({{prod_id}}, 'add')" id="custom-subtract-{{prod_id}}">
+                                        <button name="single-quick" class="btn btn-success needs-space" align="right" value="add-custom">Add other</button>
+                                    </a>
+                                </td>
+                            </tr>
+                        </table>
                         </div>
                         <form action="{{ url_for('quick_change', type='form') }}" method="POST">
                         <input name="product_id" hidden value="{{ prod_id }}">

inventory/templates/location.jinja
@@ -1,8 +1,9 @@
 {% extends 'base-template.jinja' %}
 {% block content %}
 <div class="container">
+    <h3 align="center">Location</h3>
     <table class="table">
-        <thead><th scope="col">Location ID</th><th scope="col">Location Name</th><th></th></thead>
+        <thead><th scope="col">Location ID</th><th scope="col">Location Name</th><th></th><th></th></thead>
         <tbody>
             {% for location in warehouses %}
             <tr>

inventory/templates/modal.jinja
@@ -1,9 +1,16 @@
+{% extends 'base-template.jinja' %}
+{% block content %}
 <!-- Raises alerts and messages on DB requests -->
-<div id="trans_message" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="trans_message" aria-hidden="true">
-    <div class="modal-body">
-        <p>{{ transaction_message }}</p>
+<div id="trans_message" class="container">
+        <H2>Transaction Failed</H2>
+    <div class="content">
+        <p>
+            <H3>{{ error_code }}</H3>
+            {{ transaction_message }}
+        </p>
     </div>
-    <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+    <div>
+        <a href="{{ previous }}"><button type="button" class="btn btn-default">Close</button></a>
     </div>
 </div>
+{% endblock %}

commit d99b644e13a8bcf82ade606a9d8037fda968da3c

Fix reorder logic bug
Fixes logic bug introduced in #22.

inventory/app.py
@@ -434,7 +434,7 @@ def quick_change():
                 get_state = conn.execute("SELECT been_reordered FROM products WHERE prod_id = ?", (prod_id)).fetchone()[0]
                 if get_state == 1:
                     new_state = 0
-                elif get_state == 0:
+                else:
                     new_state = 1
                 conn.execute(
                     "UPDATE products SET been_reordered = ? WHERE prod_id = ?", (new_state, prod_id)