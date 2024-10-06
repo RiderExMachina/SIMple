RiderExMachina/SIMple

commit 67324117caca9386936985237deba64fb0a6311b

Sort by location (#24)
* Add filters and quick-take bar

Add location filters to close #1

Also add UPC Quick take to close #19

Will need to do more work to get Categories working.

inventory/app.py
@@ -44,7 +44,8 @@ def init_database():
         "vendor_url TEXT, "
         "purchase_cost INTEGER, "
         "sale_price INTEGER, "
-        "unallocated_quantity INTEGER)"
+        "unallocated_quantity INTEGER, "
+        "FOREIGN KEY(location) REFERENCES location(loc_id))"
     )
     LOCATIONS = "location(loc_id INTEGER PRIMARY KEY AUTOINCREMENT, loc_name TEXT UNIQUE NOT NULL)"
     LOGISTICS = (
@@ -60,34 +61,74 @@ def init_database():
         "FOREIGN KEY(to_loc_id) REFERENCES location(loc_id))"
     )
     CATEGORIES = (
-        "categories=("
+        "category("
         "cat_id INTEGER PRIMARY KEY AUTOINCREMENT, "
         "category_name TEXT UNIQUE NOT NULL) "
         )
+    SET_CATEGORIES = (
+        "prod_categories("
+        "set_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "prod_id INTEGER NOT NULL, "
+        "cat_id INTEGER NOT NULL, "
+        "FOREIGN KEY(prod_id) REFERENCES products(prod_id), "
+        "FOREIGN KEY(cat_id) REFERENCES categories(cat_id)) "
+        )
+    SETTINGS = (
+        "settings("
+        "setting_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        "setting_name TEXT NOT NULL, "
+        "setting_val INTEGER NOT NULL) "
+        )
 
     with sqlite3.connect(DATABASE_NAME) as conn:
-        for table_definition in [PRODUCTS, LOCATIONS, LOGISTICS]:
+        for table_definition in [PRODUCTS, LOCATIONS, LOGISTICS, CATEGORIES, SET_CATEGORIES, SETTINGS]:
             conn.execute(f"CREATE TABLE IF NOT EXISTS {table_definition}")
-
 
 app.init_db = init_database
 
+def get_existing():
+    with sqlite3.connect(DATABASE_NAME) as conn:
+        existing = conn.execute("SELECT * FROM settings").fetchall()
+        if len(existing) == 0:
+            for name, value in [["loc_set", 0], ["cat_set", 0]]:
+                conn.execute("INSERT INTO settings (setting_name, setting_val) VALUES (?, ?)", (name, value))
+
+        # loc_set, loc_id | cat_set, cat_id
+        loc_set = conn.execute("SELECT setting_val FROM settings WHERE setting_name = 'loc_set'").fetchone()[0]
+        cat_set = conn.execute("SELECT setting_val FROM settings WHERE setting_name = 'cat_set'").fetchone()[0]
+    return loc_set, cat_set
+
 @app.route("/", methods=["GET"])
 def summary():
+    loc_filter, cat_filter = get_existing()
+
     with sqlite3.connect(DATABASE_NAME) as conn:
-        warehouse = conn.execute("SELECT * FROM location").fetchall()
-        products = conn.execute("SELECT * FROM products ORDER BY prod_name ASC").fetchall()
-        q_data = conn.execute(
-            "SELECT prod_name, unallocated_quantity, prod_quantity, reorder_qty, restock_qty, been_reordered FROM products"
-        ).fetchall()
+        location = conn.execute("SELECT * FROM location").fetchall()
+        if loc_filter:
+            location_selected = 1
+            print(f"{loc_filter}, {type(loc_filter)}")
+            products = conn.execute("SELECT * FROM products WHERE location = ? ORDER BY prod_name ASC", (str(loc_filter),)).fetchall()
+            loc_name = conn.execute("SELECT loc_name FROM location WHERE loc_id = ?", (str(loc_filter),)).fetchone()[0]
+        #Categories will likely be more difficult due to cross-table data
+        #if cat_filter:
+            #category_selected = 1
+            #products = conn.execute("SELECT * FROM prod_categories WHERE cat_id = ? ORDER BY prod_name ASC", (cat_filter)).fetchall()
+        else:
+            location_selected = 0
+            products = conn.execute("SELECT * FROM products ORDER BY prod_name ASC").fetchall()
+            loc_filter = ""
+            loc_name = "All Products"
 
     return render_template(
         "index.jinja",
         link=VIEWS,
         title="Summary",
-        warehouses=warehouse,
+        location_selected=location_selected,
+        filtered_loc_id= loc_filter,
+        filtered_loc_name=loc_name,
+        amount=len(products),
+        locations=location,
         products=products,
-        summary=q_data,
     )
 
 
@@ -361,6 +402,13 @@ def quick_change():
                 request.form["product_id"],
                 request.form["custom_qty"]
             )
+    if quick_change_type == "upc_search":
+        upc = request.form["quick-take-bar"]
+        if not upc:
+            return redirect(VIEWS["Summary"])
+        with sqlite3.connect(DATABASE_NAME) as conn:
+            prod_id, qty = conn.execute("SELECT prod_id, quick_take_qty FROM products WHERE prod_upc = ?", (upc, )).fetchall()[0]
+        quick_change_type = "subtract"
     else:
         prod_id, qty = (
             request.args.get("product"),
@@ -397,7 +445,6 @@ def quick_change():
                 conn.execute(
                     "UPDATE products SET been_reordered = 0, prod_quantity = ? WHERE prod_id = ?", (new_qty, prod_id)
                     )
-
 
         return redirect(VIEWS["Summary"])
 
@@ -487,6 +534,20 @@ def edit():
 
             case _:
                 return redirect(VIEWS["Summary"])
+
+@app.route('/set-filter', methods=["GET", "POST"])
+def set_filter():
+    loc_filter = request.form["location-filter"]
+    cat_filter = request.form["category-filter"]
+
+    if (loc_filter and int(loc_filter)) or int(loc_filter) == 0:
+        with sqlite3.connect(DATABASE_NAME) as conn:
+            conn.execute("UPDATE settings SET setting_val = ? WHERE setting_name = 'loc_set'", (loc_filter))
+    if (cat_filter and int(cat_filter) or cat_filter == 0):
+        with sqlite3.connect(DATABASE_NAME) as conn:
+            conn.execute("UPDATE settings SET setting_val = ? WHERE setting_name = 'cat_set'", (cat_filter))
+
+    return redirect(VIEWS["Summary"])
 
 with app.app_context():
     app.init_db()

inventory/static/main.css
@@ -1,3 +1,9 @@
+.filter-sidebar{
+  float: left;
+  padding-top: 80px;
+  padding-left: 70px;
+}
+
 .content{
     margin-left: 25px;
 }

inventory/templates/index.jinja
@@ -1,10 +1,35 @@
 {% extends 'base-template.jinja' %}
 {% block content %}
+    <div class="sidebar filter-sidebar" align="left">
+        <h4></h4>
+        <form action="{{url_for('quick_change', type='upc_search')}}" method="POST">
+            <input name="quick-take-bar" type="search" placeholder="Scan UPC Here" autofocus>
+            <input type="button" type="submit" accesskey="Enter" hidden>
+        </form>
+        <span class="needs-space"/>
+        <h4>Filters</h4>
+        <form action="{{url_for('set_filter')}}" method="POST">
+            <select name="location-filter" class="selectpicker">
+            {% if location_selected == 1 %}
+                <option value="{{ filtered_loc_id }}">{{ filtered_loc_name }}</option>
+            {% endif %}
+                <option value="0">All Products</option>
+            {% for loc_id, loc_name in locations %}
+                {% if loc_id != filtered_loc_id %}
+                <option value="{{loc_id}}">{{loc_name}}</option>
+                {% endif %}
+            {% endfor %}
+            </select>
+            <input name="category-filter" value="" hidden/>
+            <button type="submit" class="btn btn-success">Filter</button>
+        </form>
+    </div>
     <div class="container" style="float: contour;" align="center">
     <h3 align="center">Inventory</h3>
+    <h6 align="center">{{amount }} Products Shown</h6>
     <hr>
         <div class="form-horizontal" title="Item list">
-        {% if not summary %}
+        {% if not products %}
             <h3 align="center" class="font-weight-light">No Items</h3>
         {% else %}
         <div id="item-buttons">

commit b203d58e244b8fea223366fe03ff23f25854f6c5

Minor bug fixes
- Fixed logic bug introduced in #24
- Fixed return bug introduced all the way back in 3e2b7aa due to forgetting to update the "redirect" flags to the new name

inventory/app.py
@@ -382,7 +382,7 @@ def delete():
                             (displaced_qty[products_], products_),
                         )
                     conn.execute("DELETE FROM location WHERE loc_id = ?", location_id)
-                return redirect(VIEWS["Warehouses"])
+                return redirect(VIEWS["Locations"])
 
             case _:
                 return redirect(VIEWS["Summary"])
@@ -402,7 +402,7 @@ def quick_change():
                 request.form["product_id"],
                 request.form["custom_qty"]
             )
-    if quick_change_type == "upc_search":
+    elif quick_change_type == "upc_search":
         upc = request.form["quick-take-bar"]
         if not upc:
             return redirect(VIEWS["Summary"])
@@ -469,7 +469,7 @@ def edit():
                     conn.execute(
                         "UPDATE location SET loc_name = ? WHERE loc_id = ?", (loc_name, loc_id)
                     )
-                return redirect(VIEWS["Warehouses"])
+                return redirect(VIEWS["Locations"])
 
             case "product":
                 (prod_id,